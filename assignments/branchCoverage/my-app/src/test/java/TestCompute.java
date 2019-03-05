5f23632087d388001f1db7c7e10c49ffa55143e3
elifbasak <yildirimelifbasak@gmail.com>
2019-03-05 13:38:33 +0200

    compute class counter  test case

--- assignments/branchCoverage/my-app/src/test/java/TestCompute.java
+++ assignments/branchCoverage/my-app/src/test/java/TestCompute.java
@@ -9,11 +9,43 @@ public class TestCompute {
   Compute c;
 
   @Test
-  public void example() {
+  public void sizeKontrol() {
     MessageQueue mq = mock(MessageQueue.class);
 	  when(mq.size()).thenReturn(0);
     c = new Compute(mq);
 	int a =  c.countNumberOfOccurrences("hava guzel");
     assertTrue(a==-1);
   }
+	
+  @Test
+  public void containsKomtrol() {
+    MessageQueue mq = mock(MessageQueue.class);
+	  when(mq.size()).thenReturn(2);
+	   when(mq.contains(anyString())).thenReturn(false);
+    c = new Compute(mq);
+	int a =  c.countNumberOfOccurrences("hava guzel");
+    assertTrue(a==0);
+  }
+	
+  @Test
+  public void counterKomtrol() {
+    MessageQueue mq = mock(MessageQueue.class);
+	  when(mq.size()).thenReturn(2);
+	   when(mq.contains(anyString())).thenReturn(true);
+    c = new Compute(mq);
+	  when(mq.getAt(anyInt())).thenReturn("hava guzel");
+	int a =  c.countNumberOfOccurrences("hava guzel");
+    assertTrue(a==2);
+  }
+	 @Test
+  public void counterKomtrol2() {
+    MessageQueue mq = mock(MessageQueue.class);
+	  when(mq.size()).thenReturn(2);
+	   when(mq.contains(anyString())).thenReturn(true);
+    c = new Compute(mq);
+	  when(mq.getAt(anyInt())).thenReturn("abc");
+	int a =  c.countNumberOfOccurrences("hava guzel");
+    assertFalse(a==2);
+  }
+	
 }
