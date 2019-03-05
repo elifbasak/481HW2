82e3dbfbbc007d387eeb564afcf3ccfc88904deb
elifbasak <yildirimelifbasak@gmail.com>
2019-03-05 02:09:49 +0200

    compute class size  test case

--- assignments/branchCoverage/my-app/src/test/java/TestCompute.java
+++ assignments/branchCoverage/my-app/src/test/java/TestCompute.java
@@ -11,7 +11,9 @@ public class TestCompute {
   @Test
   public void example() {
     MessageQueue mq = mock(MessageQueue.class);
+	  when(mq.size()).thenReturn(0);
     c = new Compute(mq);
-    assertTrue(true);
+	int a =  c.countNumberOfOccurrences("hava guzel");
+    assertTrue(a==-1);
   }
 }
